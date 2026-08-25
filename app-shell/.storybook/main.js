53c515b5fb6acab0089112ffd2b2548cbf520ba3
jj <>
2026-08-24 21:04:46 -0700

    feat(storybook): unify storybook config and clean up component boilerplate

--- app-shell/.storybook/main.js
+++ app-shell/.storybook/main.js
@@ -1,5 +1,8 @@
 const config = {
-  stories: ['../stories/*.stories.{js,md,mdx}'],
+  stories: [
+    '../stories/*.stories.{js,md,mdx}',
+    '../src/**/stories/*.stories.js'
+  ],
 
   framework: {
     name: '@web/storybook-framework-web-components',
--- app-shell/src/organisms/appeal-list/.storybook/main.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  stories: ['../stories/*.stories.{js,md,mdx}'],
-  framework: {
-    name: '@web/storybook-framework-web-components',
-  },
-};
-
-export default config;
